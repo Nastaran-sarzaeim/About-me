3e1836078daa8ef42091c76d83c1f7a0388874d0
Nastaran <nastaransarzaeim03@gmail.com>
2024-04-22 11:59:47 +0330

    feat : Create the MyFriends component and add styling to it

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
+import MyFriends from "./MyFriends";
 
 function HomePage() {
   return (
@@ -8,6 +9,7 @@ function HomePage() {
       <div className="h-screen">
         <Header />
         <Content />
+        <MyFriends/>
         <hr />
         <Footer/>
       </div>
